ac13dea2997ff445a4ed0dc25e624a67abcd6548
Alejandro2718 <alejandrojimenez2718@gmail.com>
2025-05-17 21:35:27 +0200

    task1

---
+++ src/main/java/com/Alejandro2718/app_with_Spring_Boot/AppWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.Alejandro2718.app_with_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Alejandro2718/app_with_Spring_Boot/AppWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alejandro2718.app_with_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
